d00c1f074b95e00d40a3645c3e1e60d16a23ba8d
Jake BS <j.bailey-saltmarsh+github@outlook.com>
2021-07-05 23:42:02 +0100

    created uni page

---
+++ js/buttons.js
@@ -0,0 +1,15 @@
+let edu_button = document.getElementById("education-button");
+let crs_button = document.getElementById("courses-button");
+let wrk_button = document.getElementById("work-button");
+
+edu_button.onclick = () => {
+    window.location.href = "uni.html"
+}
+
+crs_button.onclick = () => {
+    window.location.href = "courses.html"
+}
+
+wrk_button.onclick = () => {
+    window.location.href = "work.html"
+}
