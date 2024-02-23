d392d6fc3a0b7fc1ceb3ba26ec1195f9209db9d3
JakeR27 <61947903+JakeR27@users.noreply.github.com>
2024-02-23 17:03:15 +0000

    updated with other letters

--- js/letter-data.js
+++ js/letter-data.js
@@ -1,5 +1,36 @@
 const letters = [];
 letters.push({
+    id: 0,
+    title: "Pilot",
+    date: "16/09/23",
+    content: [
+        "Dear Abby,",
+        "I can imagine that you weren't expecting me to put anything like this together, let alone write a letter to you! I would like to voice how much I enjoyed spending time with you today, I honestly haven't had this much fun for a long time. Thank you for being my Ilkley Moor tour guide",
+        "There were so many things I should have said to you today in the car to make how I feel as clear as possible. However, you truly made me me speechless. I absolutely love spending time with you and I believe you are genuinely amazing. What's more, I think, in fact I know, you are funny, smart, caring, kind, passionate, witty and most of all beautiful!",
+        "I've never written a letter like this, and didn't particularly enjoy english lessons, so I ask for you forgiveness if this letter is not written like a standard letter. On the other hand, you are exceptional, so it only makes sense you don't receive a standard letter.",
+        "Hopefully, by the time you receive this letter we will have already organised to see eachother again soon. However, if not, I would like to invite you a guided tour of your choosing on the 30th September. You can reply by sending a letter back to me at the address below, or by texting me.",
+        "Do you know how beautiful you are?"
+   ],
+    signoff: "Yours,",
+    signature: "Jake",
+    ps: "P.S. My address:<br>3 Beamsley Terrace<br>Burley, Leeds<br>LS6 1LP",
+    author: "jake"
+})
+letters.push({
+    id: 1,
+    title: "Girlfriend 😮",
+    date: "02/10/23",
+    content: [
+        "Hey!",
+        "Wow, I can't believe I'm lucky enough to call you my girlfriend! Every time I think of you I'm flushed with a bout of uncontrollable grinning - the office must think I'm crazy! I think with the amount I now smile I could pass as The Joker.",
+        "I think now is the perfect time to inform you that I'm not going anywhere, and it would be impossible to change my mind. So, I'm sorry to formally inform you that, you're stuck with me now - your 30 second cancellation period has certainly ended, and you may no longer go back on your previous decision ;)",
+        "You should know that I feel a constant and compelling urge to share how excited I am to see you and spend time with you. If I siad it as many times as I thought it, our chats would be purely streams of my continued excitement. Here I have managed to limit myself to only two sentences. An achievement! It sometimes helps to think of activities I cannot wait to do with you: three peaks, karting, park run, climbing, travelling, camping, chess, escape rooms, movie marathons, talking, relaxing, sleeping and of course endless more tour guides! :)",
+    ],
+    signoff: "Most certainly and always, yours,",
+    signature: "Jake",
+    author: "jake"
+})
+letters.push({
     id: 2,
     title: "Christmasss",
     date: "25/12/23",
@@ -9,7 +40,23 @@ letters.push({
         "Now, presents! You will find someone Loyal, something Original, something Versatile and something Extra. Merry Christmas!",
     ],
     signoff: "With lots of love,<br>Always Yours,",
-    signature: "Jake xxx"
+    signature: "Jake xxx",
+    author: "jake"
+})
+letters.push({
+    id: 3,
+    title: "Quarterversary",
+    date: "3 months",
+    content: [
+        "Dear Abby,",
+        "How does it feel to have been stuck with me for <span class='underline'>three</span> whole months? My head can't quite comprehend how quickly it's gone, yet it feels like we've been together forever. What's more, I'm the happiest I've been in a long time - so thank you for all the big things, all the little things, and everything in between. You really are a star!",
+        "I've been thinking about next year and the things we could do, places we could visit and all the tours we can run together. There's so much adventure we're going to have together - I can't wait",
+        "As for this year, we've done so many things I never expected to do. You've pushed me to be the best version of myself without either of us realising. From begrudgingly swimming in the freezing sea to walking everywhere at break neck speed. I love every second of every minute I spend with you.",
+    ],
+    signoff: "I love you,<br>Always Yours,",
+    signature: "Jake",
+    ps: "P.S. Maybe your New Years Resolution can be that you walk <span class='underline'>slightly</span> slower, so old men like me can keep up ;)",
+    author: "jake"
 })
 letters.push({
     id: 4,
@@ -20,7 +67,8 @@ letters.push({
         "You are truly amazing. Will you allow me the honour of being your valentines this year? &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; YES :) &nbsp;&nbsp; NO :(",
     ],
     signoff: "Always Yours,",
-    signature: "Jake xxx"
+    signature: "Jake xxx",
+    author: "jake"
 })
 letters.push({
     id: 5,
@@ -36,5 +84,6 @@ letters.push({
         "I love you.",
     ],
     signoff: "Always Yours,",
-    signature: "Jake xxx"
+    signature: "Jake xxx",
+    author: "abby"
 })
--- js/letter-load.js
+++ js/letter-load.js
@@ -1,6 +1,6 @@
 function load() {
     for (let i = 0; i < letters.length; i++) {
-        addEnvelope(letters[i].id, letters[i].title)
+        addEnvelope(letters[i].id, letters[i].title, letters[i].author || "jake")
     }
 
     let envs = document.getElementsByClassName('envelope')
@@ -12,11 +12,11 @@ function load() {
     }
 }
 
-function addEnvelope(id, title) {
+function addEnvelope(id, title, author) {
     let envs = document.getElementById('envelope-container')
     envs.innerHTML +=
         `<div class="envelope" data-id=${id}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <svg class="author-${author}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
             </svg>
             <p>${title}</p>
@@ -47,6 +47,11 @@ function displayLetter(id) {
     }
     content.innerHTML = newContent;
     content.innerHTML = content.innerHTML + `<p>${letter.signoff}<br><span class="signature">${letter.signature}</span></p>`;
+
+    if (letter.ps && letter.ps !== "") {
+        content.innerHTML = content.innerHTML + `<p>${letter.ps}</p>`;
+    }
+
 
     jump("letter-header");
 }
